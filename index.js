af67a80c51fbbfb7d3cb245c50a8d85bd5e3641f
bhaveshcodes <120417108+bhaveshcodes@users.noreply.github.com>
2022-12-17 22:07:17 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,18 +5,12 @@ const app = express();
 const port = 9000;
 
 app.use("/", (req, res) => {
-function fetch(){
-const result = await axios(`https://randomuser.me/api/`)
-console.log(result);
-    
-}
-fetch()
-res.send(result)
-  // res.json({ message: "Hello Vercel!" });
+  res.json({ message: "Hello Vercel" });
 });
 
 app.listen(9000, async() => {
-     
+    // const result = await axios(`https://randomuser.me/api/`)
+    // console.log(result);
   console.log(`Server is running on Port ${port}`);
 });
 
