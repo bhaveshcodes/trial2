092127f36500deddc8ed86e13e10649c4aadc7f0
bhaveshcodes <120417108+bhaveshcodes@users.noreply.github.com>
2022-12-17 22:05:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ const app = express();
 const port = 9000;
 
 app.use("/", (req, res) => {
-  res.json({ message: "Hello Vercel!" });
+function fetch(){
+const result = await axios(`https://randomuser.me/api/`)
+console.log(result);
+    
+}
+fetch()
+res.send(result)
+  // res.json({ message: "Hello Vercel!" });
 });
 
 app.listen(9000, async() => {
-     const result = await axios(`https://randomuser.me/api/`)
-    console.log(result);
+     
   console.log(`Server is running on Port ${port}`);
 });
 
