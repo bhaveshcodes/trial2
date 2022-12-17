30ffe97f73461145d3dd2277da3e3330be93b5cc
bhaveshcodes <120417108+bhaveshcodes@users.noreply.github.com>
2022-12-17 21:58:58 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+const port = 9000;
+
+app.use("/", (req, res) => {
+  res.json({ message: "Hello Vercel" });
+});
+
+app.listen(9000, async() => {
+    // const result = await axios(`https://randomuser.me/api/`)
+    // console.log(result);
+  console.log(`Server is running on Port ${port}`);
+});
+
