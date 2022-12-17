8551b7b5f020553c11077ed436c57135c6314ff5
bhaveshcodes <120417108+bhaveshcodes@users.noreply.github.com>
2022-12-17 23:45:00 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -54,8 +54,8 @@ console.log("here");
         // const result = await axios('https://randomuser.me/api/')
         // console.log("result2");
         fetch('https://randomuser.me/api/')
-            .then((response) => response.json())
-            .then((data) => res.json(data));
+            // .then((response) => response.json())
+            .then((data) => res.send("data"));
     }
 
     //   res.json({ message: "result" });
